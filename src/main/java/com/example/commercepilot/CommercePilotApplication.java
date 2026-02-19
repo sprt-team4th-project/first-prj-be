384859db758aca6c418ab91f2a179eefa382d834
fryzke <fryekdy@gmail.com>
2026-02-19 14:49:47 +0900

    프로젝트 초기 설정 및 환경설정

---
+++ src/main/java/com/example/commercepilot/CommercePilotApplication.java
@@ -0,0 +1,13 @@
+package com.example.commercepilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommercePilotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommercePilotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/commercepilot/CommercePilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.commercepilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommercePilotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
