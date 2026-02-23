59b824c6230719ad5ce80cea48f6444df283a511
jiiimni <leejimin8951@naver.com>
2026-02-23 09:27:03 +0900

    refactor: Admin 엔티티에 프로필 수정 및 비밀번호 변경 메서드 추가

--- src/main/java/com/example/commercepilot/admin/entity/Admin.java
+++ src/main/java/com/example/commercepilot/admin/entity/Admin.java
@@ -74,4 +74,14 @@ public class Admin {
     public void changeStatus(AdminStatus status) {
         this.status = status;
     }
+
+    public void updateProfile(String name, String email, String callNumber) {
+        this.adminName = name;
+        this.email = email;
+        this.callNumber = callNumber;
+    }
+
+    public void changePassword(String encodedPassword) {
+        this.password = encodedPassword;
+    }
 }
