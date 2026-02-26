020abaefd48b377b776a691a142f02fcf6f176df
hyeonmin02 <hyenmin5@gmail.com>
2026-02-26 17:05:35 +0900

    fix: Product Session name 상수변환

--- src/main/java/com/example/commercepilot/product/controller/ProductController.java
+++ src/main/java/com/example/commercepilot/product/controller/ProductController.java
@@ -14,6 +14,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import static com.example.commercepilot.admin.config.SessionConst.LOGIN_ADMIN;
+
 @RestController
 @RequestMapping("/api/products")
 @RequiredArgsConstructor
@@ -24,7 +26,7 @@ public class ProductController {
 
     @PostMapping // 상품 생성
     public ResponseEntity<ApiResponse<ProductCreateResponse>> createProduct(
-            @SessionAttribute(name = SessionConst.LOGIN_ADMIN, required = false) LoginAdmin loginAdmin,
+            @SessionAttribute(name = LOGIN_ADMIN, required = false) LoginAdmin loginAdmin,
             @Valid @RequestBody ProductCreateRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(ApiResponse.success(HttpStatus.CREATED, productCommandService.createProduct(loginAdmin, request)));
@@ -32,7 +34,7 @@ public class ProductController {
 
     @PatchMapping("/{productId}") // 상품 수정 (상품명, 카테고리, 가격)
     public ResponseEntity<ApiResponse<ProductUpdateResponse>> updateProduct(
-            @SessionAttribute(name = "loginAdmin", required = false) LoginAdmin loginAdmin,
+            @SessionAttribute(name = LOGIN_ADMIN , required = false) LoginAdmin loginAdmin,
             @Valid @RequestBody ProductUpdateRequest request,
             @PathVariable Long productId) {
         return ResponseEntity.ok
@@ -54,7 +56,7 @@ public class ProductController {
 
     @DeleteMapping("/{productId}") // 상품 단건 삭제
     public ResponseEntity<ApiResponse<Void>> deleteProduct(
-            @SessionAttribute(name = "loginAdmin", required = false) LoginAdmin loginAdmin,
+            @SessionAttribute(name = LOGIN_ADMIN, required = false) LoginAdmin loginAdmin,
             @PathVariable Long productId) {
         productCommandService.deleteProduct(loginAdmin, productId);
         return ResponseEntity.ok(ApiResponse.success(HttpStatus.OK));
@@ -62,7 +64,7 @@ public class ProductController {
 
     @PatchMapping("/{productId}/stock") // 재고 값 변경
     public ResponseEntity<ApiResponse<StockChangeResponse>> changeStock(
-            @SessionAttribute(name = "loginAdmin", required = false) LoginAdmin loginAdmin,
+            @SessionAttribute(name = LOGIN_ADMIN, required = false) LoginAdmin loginAdmin,
             @PathVariable Long productId,
             @RequestBody StockChangeRequest request) {
         return ResponseEntity.ok(ApiResponse.success(HttpStatus.OK,
@@ -71,7 +73,7 @@ public class ProductController {
 
     @PatchMapping("/{productId}/stock/increase") // 기존 재고 추가
     public ResponseEntity<ApiResponse<StockChangeResponse>> increaseStock(
-            @SessionAttribute(name = "loginAdmin", required = false) LoginAdmin loginAdmin,
+            @SessionAttribute(name = LOGIN_ADMIN, required = false) LoginAdmin loginAdmin,
             @PathVariable Long productId,
             @RequestBody StockAmountRequest request) {
         return ResponseEntity.ok(ApiResponse.success(HttpStatus.OK,
@@ -80,7 +82,7 @@ public class ProductController {
 
     @PatchMapping("/{productId}/stock/decrease") // 기존 재고 감소
     public ResponseEntity<ApiResponse<StockChangeResponse>> decreaseStock(
-            @SessionAttribute(name = "loginAdmin", required = false) LoginAdmin loginAdmin,
+            @SessionAttribute(name = LOGIN_ADMIN, required = false) LoginAdmin loginAdmin,
             @PathVariable Long productId,
             @RequestBody StockAmountRequest request) {
         return ResponseEntity.ok(ApiResponse.success(HttpStatus.OK,
@@ -89,7 +91,7 @@ public class ProductController {
 
     @PatchMapping("/{productId}/status/discontinue") // 재고상태 단종으로 변경
     public ResponseEntity<ApiResponse<Void>> discontinueProduct(
-            @SessionAttribute(name = "loginAdmin", required = false) LoginAdmin loginAdmin,
+            @SessionAttribute(name = LOGIN_ADMIN, required = false) LoginAdmin loginAdmin,
             @PathVariable Long productId
     ) {
         productCommandService.discontinueProduct(loginAdmin, productId);
